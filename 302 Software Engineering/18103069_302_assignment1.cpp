00eb3ec81dfe0e4efc8f231a81a2093c1420c9cb
gupavi <61126917+gupavi@users.noreply.github.com>
2020-08-21 16:46:54 +0530

    Assignment 1 amendments

--- 302 Software Engineering/18103069_302_assignment1.cpp
+++ 302 Software Engineering/18103069_302_assignment1.cpp
@@ -3,6 +3,7 @@ Lab Assignment 1: Write a program which accepts the three sides of a triangle an
 Abhinav Gupta_18103069
 */
 #include<iostream>
+#include<cmath>
 using namespace std;
 int main(){
 	float a,b,c;
@@ -14,19 +15,38 @@ int main(){
 	if (bad)
 		cout << "Enter Valid lengths (Float values allowed)" << endl;
 	else{
+
 		//checking if lengths are positive and
 		//checking if triangle can be formed
 		if(a+b>c && b+c>a && a+c>b && a>0 && b>0 && c>0){
-	
+			float a_squared = pow(a,2);
+			float b_squared = pow(b,2);
+			float c_squared = pow(c,2);
+			//cout<<a_squared<<" "<<b_squared<<" "<<c_squared<<endl;
 			if(a==b || b==c || a==c){
 				//isosceles confirmed
 				if(a==b && b==c)
 					cout<<"Equilateral Triangle"<<endl;
-				else
-					cout<<"Isosceles Triangle"<<endl;
+				else{
+				if((a_squared + b_squared == c_squared)
+					||
+					(b_squared + c_squared == a_squared)
+					||
+					(c_squared + a_squared == b_squared))
+						cout<<"Right Angled Isosceles Triangle"<<endl;
+					else
+						cout<<"Isosceles Triangle"<<endl;
+				}
 			}
 			else{
-				cout<<"Scalene Triangle"<<endl;
+				if((a_squared + b_squared == c_squared)
+					||
+					(b_squared + c_squared == a_squared)
+					||
+					(c_squared + a_squared == b_squared))
+					cout<<"Right Angled Triangle"<<endl;
+				else
+					cout<<"Scalene Triangle"<<endl;
 			}
 		}
 		//either side length is negative or sum property of triangle isn't satisfied
